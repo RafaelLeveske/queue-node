68cccc6d78b33cbf0a4cf63b48350f8d56ddc5bf
Rafael Vieira <rafaelleveske@gmail.com>
2021-02-17 19:49:45 -0300

    Refatorando

--- src/shared/infra/http/queue.ts
+++ src/shared/infra/http/queue.ts
@@ -5,11 +5,3 @@ dotenv.config();
 import Queue from '@modules/users/lib/Queue';
 
 Queue.process();
-
-// worker.on('completed', job => {
-//   console.log(`${job.id} has completed!`);
-// });
-
-// worker.on('failed', (job, err) => {
-//   console.log(`${job.id} has failed with ${err.message}`);
-// });
